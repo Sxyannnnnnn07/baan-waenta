a8d520739484329b9238fea420467e13351f0cc0
Sahiram Jehha <sahiramm12345@gmail.com>
2026-08-05 01:12:57 +0700

    แก้

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(helmet({
         directives: {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", "'unsafe-inline'", "https://unpkg.com", "https://cdn.jsdelivr.net"],
+            scriptSrcAttr: ["'unsafe-inline'"],
             styleSrc: ["'self'", "'unsafe-inline'", "https://unpkg.com", "https://fonts.googleapis.com"],
+            styleSrcAttr: ["'unsafe-inline'"],
             fontSrc: ["'self'", "https://fonts.gstatic.com"],
             imgSrc: ["'self'", "data:", "blob:", "https://*"],
             connectSrc: ["'self'", "https://*", "wss://*"],
